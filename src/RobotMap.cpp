cd9034f1a1fb5d7c49447a9bb1a966a3ee03702e
2826WaveRobotics <kelliewatters@gmail.com>
2018-03-12 15:26:43 -0500

    Revert "Revert "3/12/18 Auto errors""
    This reverts commit 63a3581c6080ed6fc027758d20a694ebfcf13494.

--- src/RobotMap.cpp
+++ src/RobotMap.cpp
@@ -24,9 +24,9 @@ int klaw = 3;
 
 
 //digital input
-int intakeSwitch = 7;
+int intakeSwitch = 6;
 int elbowZero = 4;
-int elevatorZero = 8;
+int elevatorZero = 8; //This is what we are using
 int elevatorHigh = 5;
 
 //encoder
